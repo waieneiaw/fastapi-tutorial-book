adbd5a727d4342581ca025db9aeb4b9c18cf4482
waieneiaw <15177530+waieneiaw@users.noreply.github.com>
2023-01-15 06:06:18 +0000

    feat: add get_user

--- src/blog/main.py
+++ src/blog/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends, status, HTTPException
 from typing import Optional, List
 from sqlalchemy.orm import Session
 from . import models
-from .schemas import Blog, ShowBlog, User
+from .schemas import Blog, ShowBlog, User, ShowUser
 from .types import JsonType, HTTPResponse
 from .models import Base
 from .database import engine, sessionLocal
@@ -114,3 +114,15 @@ def create_user(request: User, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get("/user/{id}", response_model=ShowUser)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with the id {id} is not available",
+        )
+
+    return user
--- src/blog/schemas.py
+++ src/blog/schemas.py
@@ -22,3 +22,14 @@ class User(BaseModel):
     name: str
     email: str
     password: str
+
+    class Config:
+        orm_mode = True
+
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+
+    class Config:
+        orm_mode = True
